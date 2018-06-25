ce4abed0faeb160205a1ce4f428bae0e87508def
TheAssassin <theassassin@assassinate-you.net>
2018-06-25 17:19:53 +0200

    Improve CMake variable name

--- cmake/Modules/FindLibMagic.cmake
+++ cmake/Modules/FindLibMagic.cmake
@@ -12,16 +12,16 @@ if(NOT LIBMAGIC_A)
     message(FATAL_ERROR "libmagic.a not found")
 endif()
 
-find_path(LIBMAGIC_MAGIC_H
+find_path(LIBMAGIC_MAGIC_H_DIR
     NAMES magic.h
     HINTS ${CMAKE_INSTALL_PREFIX}
     PATH_SUFFIXES include include/linux
     )
 
-if(NOT LIBMAGIC_MAGIC_H)
+if(NOT LIBMAGIC_MAGIC_H_DIR)
     message(FATAL_ERROR "magic.h not found")
 endif()
 
 add_library(libmagic_static INTERFACE IMPORTED)
-set_property(TARGET libmagic_static PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${LIBMAGIC_MAGIC_H})
+set_property(TARGET libmagic_static PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${LIBMAGIC_MAGIC_H_DIR})
 set_property(TARGET libmagic_static PROPERTY INTERFACE_LINK_LIBRARIES ${LIBMAGIC_A};ZLIB::ZLIB)
