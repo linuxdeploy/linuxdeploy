2b94a4b408591a2f811d35b1126663feb0ad9583
TheAssassin <theassassin@assassinate-you.net>
2018-06-29 12:31:20 +0200

    Improve FindLibMagic

--- cmake/Modules/FindLibMagic.cmake
+++ cmake/Modules/FindLibMagic.cmake
@@ -3,9 +3,6 @@ cmake_minimum_required(VERSION 3.3)
 
 message(STATUS "Searching for libmagic")
 
-# on Debian-ish systems, libmagic depends on zlib
-find_package(ZLIB REQUIRED)
-
 find_library(LIBMAGIC_A libmagic.a)
 
 if(NOT LIBMAGIC_A)
@@ -16,11 +13,14 @@ find_path(LIBMAGIC_MAGIC_H_DIR
     NAMES magic.h
     HINTS ${CMAKE_INSTALL_PREFIX}
     PATH_SUFFIXES include include/linux
-    )
+)
 
 if(NOT LIBMAGIC_MAGIC_H_DIR)
     message(FATAL_ERROR "magic.h not found")
 endif()
+
+# on Debian-ish systems, libmagic depends on zlib
+find_package(ZLIB REQUIRED)
 
 add_library(libmagic_static INTERFACE IMPORTED)
 set_property(TARGET libmagic_static PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${LIBMAGIC_MAGIC_H_DIR})
