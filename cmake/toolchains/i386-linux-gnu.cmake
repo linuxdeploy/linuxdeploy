8a31d3d677ae209521be049e5dbf17c4a35addb9
TheAssassin <theassassin@assassinate-you.net>
2018-10-24 14:07:55 +0200

    Fix cross-compiling toolchain

--- cmake/toolchains/i386-linux-gnu.cmake
+++ cmake/toolchains/i386-linux-gnu.cmake
@@ -3,3 +3,6 @@ set(CMAKE_SYSTEM_PROCESSOR i386 CACHE STRING "" FORCE)
 
 set(CMAKE_C_FLAGS "-m32" CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS "-m32" CACHE STRING "" FORCE)
+
+# https://gitlab.kitware.com/cmake/cmake/issues/16920#note_299077
+set(THREADS_PTHREAD_ARG "2" CACHE STRING "Forcibly set by CMakeLists.txt" FORCE)
