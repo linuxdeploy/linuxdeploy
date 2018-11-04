7318d68c9632c746115ad1247cc346c958755416
Alexis Lopez Zubieta <azubieta90@gmail.com>
2018-11-04 14:24:04 -0600

    Move deployAppDirRootFiles function into a separated file to allow unit testing

---
+++ src/linuxdeploy.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include <boost/filesystem/path.hpp>
+
+#include <linuxdeploy/core/appdir.h>
+#include <linuxdeploy/core/log.h>
+#include <linuxdeploy/util/util.h>
+#include <args.hxx>
+
+int deployAppDirRootFiles(args::ValueFlagList<std::string>& desktopFilePaths,
+                          args::ValueFlag<std::string>& customAppRunPath,
+                          linuxdeploy::core::appdir::AppDir& appDir);
