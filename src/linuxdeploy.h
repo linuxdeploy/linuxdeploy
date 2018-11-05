0e92331a12180c03a8d2e90ac7f0827979cec75f
Alexis Lopez Zubieta <azubieta90@gmail.com>
2018-11-05 16:13:05 -0600

    Document linuxdeploy::deployAppDirRootFiles

--- src/linuxdeploy.h
+++ src/linuxdeploy.h
@@ -6,6 +6,15 @@
 #include "linuxdeploy/core/appdir.h"
 
 namespace linuxdeploy {
+    /**
+     * Deploy the application ".deskop", icon, and runnable files in the AppDir root path. According to the
+     * AppDir spec at: https://docs.appimage.org/reference/appdir.html
+     *
+     * @param desktopFilePaths to be deployed in the AppDir root
+     * @param customAppRunPath AppRun to be used, if empty the application executable will be used instead
+     * @param appDir
+     * @return true on success otherwise false
+     */
     bool deployAppDirRootFiles(std::vector<std::string> desktopFilePaths, std::string customAppRunPath,
                                linuxdeploy::core::appdir::AppDir& appDir);
 }
