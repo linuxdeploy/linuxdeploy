11bbe4d219c8729aaf7e1d7f23d540cc5f18cae4
TheAssassin <theassassin@assassinate-you.net>
2018-11-09 16:04:28 +0100

    Rewrite DesktopFile using own parser

--- include/linuxdeploy/core/desktopfile.h
+++ include/linuxdeploy/core/desktopfile.h
@@ -26,19 +26,47 @@ namespace linuxdeploy {
             private:
                     // private data class pattern
                     class PrivateData;
-                    PrivateData* d;
+                    std::shared_ptr<PrivateData> d;
 
                 public:
                     // default constructor
                     DesktopFile();
 
                     // construct from existing desktop file
-                    // file must exist
+                    // file must exist, otherwise std::runtime_error is thrown
                     explicit DesktopFile(const boost::filesystem::path& path);
+
+                    // construct by reading an existing stream
+                    // file must exist, otherwise std::runtime_error is thrown
+                    explicit DesktopFile(std::istream& is);
+
+                    // copy constructor
+                    DesktopFile(const DesktopFile& other);
+
+                    // copy assignment constructor
+                    DesktopFile& operator=(const DesktopFile& other);
+
+                    // move assignment operator
+                    DesktopFile& operator=(DesktopFile&& other) noexcept;
+
+                    // equality operator
+                    bool operator==(const DesktopFile& other);
+
+                    // inequality operator
+                    bool operator!=(const DesktopFile& other);
+
+            public:
+                    // returns true if a file has been loaded, false otherwise
+                    bool isEmpty() const;
 
                     // read desktop file
                     // sets path associated with this file
-                    bool read(const boost::filesystem::path& path);
+                    // throws exceptions in case of issues, see DesktopFileReader for more information
+                    void read(const boost::filesystem::path& path);
+
+                    // read desktop file from existing stream
+                    // throws exceptions in case of issues, see DesktopFileReader for more information
+                    void read(std::istream& is);
 
                     // get path associated with this file
                     boost::filesystem::path path() const;
@@ -55,7 +83,13 @@ namespace linuxdeploy {
 
                     // save desktop file to path
                     // does not change path associated with desktop file
+                    // throws exceptions in case of errors, see DesktopFileWriter::save(...) for more information
                     bool save(const boost::filesystem::path& path) const;
+
+                    // save desktop file to ostream
+                    // does not change path associated with desktop file
+                    // throws exceptions in case of errors, see DesktopFileWriter::save(...) for more information
+                    bool save(std::ostream& os) const;
 
                     // check if entry exists in given section and key
                     bool entryExists(const std::string& section, const std::string& key) const;
@@ -65,10 +99,25 @@ namespace linuxdeploy {
                     // returns true (and populates value) if the key exists, false otherwise
                     bool getEntry(const std::string& section, const std::string& key, std::string& value) const;
 
+                    // get key from desktop file
+                    // an std::string passed as value parameter will be populated with the contents
+                    // returns true (and populates value) if the key exists, false otherwise
+                    bool getEntry(const std::string& section, const std::string& key, DesktopFileEntry& value) const;
+
                     // add key to section in desktop file
                     // the section will be created if it doesn't exist already
                     // returns true if an existing key was overwritten, false otherwise
                     bool setEntry(const std::string& section, const std::string& key, const std::string& value);
+
+                    // add key to section in desktop file
+                    // the section will be created if it doesn't exist already
+                    // returns true if an existing key was overwritten, false otherwise
+                    bool setEntry(const std::string& section, const DesktopFileEntry& entry);
+
+                    // add key to section in desktop file
+                    // the section will be created if it doesn't exist already
+                    // returns true if an existing key was overwritten, false otherwise
+                    bool setEntry(const std::string& section, const DesktopFileEntry&& entry);
 
                     // create common application entries in desktop file
                     // returns false if one of the keys exists and was left unmodified
--- src/core/desktopfile.cpp
+++ src/core/desktopfile.cpp
@@ -1,9 +1,10 @@
-// library headers
-#include <INI.h>
-
 // local headers
+#include <linuxdeploy/core/desktopfile.h>
+
 #include "linuxdeploy/core/desktopfile.h"
 #include "linuxdeploy/core/log.h"
+#include "desktopfilereader.h"
+#include "desktopfilewriter.h"
 
 using namespace linuxdeploy::core;
 using namespace linuxdeploy::core::log;
@@ -16,36 +17,73 @@ namespace linuxdeploy {
             class DesktopFile::PrivateData {
                 public:
                     bf::path path;
-                    INI<> ini;
+                    sections_t data;
 
                 public:
-                    PrivateData() : path(), ini("", false) {};
+                    PrivateData() = default;
+
+                    void copyData(const std::shared_ptr<PrivateData>& other) {
+                        path = other->path;
+                        data = other->data;
+                    }
+
+            public:
+                bool isEmpty() const {
+                    return data.empty();
+                }
             };
 
-            DesktopFile::DesktopFile() {
-                d = new PrivateData();
-            }
+            DesktopFile::DesktopFile() : d(std::make_shared<PrivateData>()) {}
 
             DesktopFile::DesktopFile(const bf::path& path) : DesktopFile() {
-                if (!read(path))
-                    throw std::runtime_error("Failed to read desktop file");
+                // will throw exceptions in case of issues
+                read(path);
             };
 
-            bool DesktopFile::read(const boost::filesystem::path& path) {
+            DesktopFile::DesktopFile(std::istream& is) : DesktopFile() {
+                // will throw exceptions in case of issues
+                read(is);
+            };
+
+            // copy constructor
+            DesktopFile::DesktopFile(const DesktopFile& other) : DesktopFile() {
+                d->copyData(other.d);
+            }
+
+            // copy assignment constructor
+            DesktopFile& DesktopFile::operator=(const DesktopFile& other) {
+                if (this != &other) {
+                    d->copyData(other.d);
+                }
+
+                return *this;
+            }
+
+            // move assignment operator
+            DesktopFile& DesktopFile::operator=(DesktopFile&& other) noexcept {
+                if (this != &other) {
+                    d = other.d;
+                    other.d = nullptr;
+                }
+                return *this;
+            }
+
+            void DesktopFile::read(const boost::filesystem::path& path) {
                 setPath(path);
 
+                // clear data before reading a new file
                 clear();
 
-                // nothing to do
-                if (!bf::exists(path))
-                    return true;
+                DesktopFileReader reader(path);
+                d->data = reader.data();
+            }
 
-                std::ifstream ifs(path.string());
-                if (!ifs)
-                    return false;
+            void DesktopFile::read(std::istream& is) {
+                // clear data before reading a new file
+                clear();
 
-                d->ini.parse(ifs);
-                return true;
+                DesktopFileReader reader(is);
+                d->data = reader.data();
             }
 
             boost::filesystem::path DesktopFile::path() const {
@@ -56,8 +94,12 @@ namespace linuxdeploy {
                 d->path = path;
             }
 
+            bool DesktopFile::isEmpty() const {
+                return d->isEmpty();
+            }
+
             void DesktopFile::clear() {
-                d->ini.clear();
+                d->data.clear();
             }
 
             bool DesktopFile::save() const {
@@ -65,26 +107,50 @@ namespace linuxdeploy {
             }
 
             bool DesktopFile::save(const boost::filesystem::path& path) const {
-                return d->ini.save(path.string());
+                DesktopFileWriter writer(d->data);
+                writer.save(path);
+
+                return true;
+            }
+
+            bool DesktopFile::save(std::ostream& os) const {
+                DesktopFileWriter writer(d->data);
+                writer.save(os);
+
+                return true;
             }
 
             bool DesktopFile::entryExists(const std::string& section, const std::string& key) const {
-                if (!d->ini.select(section))
+                auto it = d->data.find(section);
+                if (it == d->data.end())
                     return false;
 
-                std::string absolutelyUnlikeValue = "<>!§$%&/()=?+'#-.,_:;'*¹²³½¬{[]}^°|";
-
-                auto value =  d->ini.get<std::string, std::string, std::string>(section, key, absolutelyUnlikeValue);
-
-                return value != absolutelyUnlikeValue;
+                return (it->second.find(key) != it->second.end());
             }
 
             bool DesktopFile::setEntry(const std::string& section, const std::string& key, const std::string& value) {
                 // check if value exists -- used for return value
                 auto rv = entryExists(section, key);
 
-                // set key
-                d->ini[section][key] = value;
+                d->data[section][key] = std::move(DesktopFileEntry(key, value));
+
+                return rv;
+            }
+
+            bool DesktopFile::setEntry(const std::string& section, const DesktopFileEntry& entry) {
+                // check if value exists -- used for return value
+                auto rv = entryExists(section, entry.key());
+
+                d->data[section][entry.key()] = entry;
+
+                return rv;
+            }
+
+            bool DesktopFile::setEntry(const std::string& section, const DesktopFileEntry&& entry) {
+                  // check if value exists -- used for return value
+                auto rv = entryExists(section, entry.key());
+
+                d->data[section][entry.key()] = entry;
 
                 return rv;
             }
@@ -93,10 +159,16 @@ namespace linuxdeploy {
                 if (!entryExists(section, key))
                     return false;
 
-                if (!d->ini.select(section))
+                value = d->data[section][key].value();
+
+                return true;
+            }
+
+            bool DesktopFile::getEntry(const std::string& section, const std::string& key, DesktopFileEntry& entry) const {
+                if (!entryExists(section, key))
                     return false;
 
-                value = d->ini.get(key);
+                entry = d->data[section][key];
                 return true;
             }
 
@@ -133,6 +205,14 @@ namespace linuxdeploy {
             bool DesktopFile::validate() const {
                 // FIXME: call desktop-file-validate
                 return true;
+            }
+
+            bool DesktopFile::operator==(const DesktopFile& other) {
+                return d->data == other.d->data;
+            }
+
+            bool DesktopFile::operator!=(const DesktopFile& other) {
+                return !operator==(other);
             }
         }
     }
