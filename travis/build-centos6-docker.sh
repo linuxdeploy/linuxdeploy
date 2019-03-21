4f44dcf954f8994438f3c462d490d1f4e9b442c0
TheAssassin <theassassin@assassinate-you.net>
2019-03-21 11:28:15 +0100

    Fix Docker build script path issue

--- travis/build-centos6-docker.sh
+++ travis/build-centos6-docker.sh
@@ -5,9 +5,9 @@ set -xe
 old_cwd=$(readlink -f .)
 here=$(readlink -f $(dirname "$0"))
 
-DOCKERFILE=Dockerfile.centos6
+DOCKERFILE="$here"/Dockerfile.centos6
 IMAGE=linuxdeploy-build-centos6
 
-docker build -f "$DOCKERFILE" -t "$IMAGE" .
+(cd "$here" && docker build -f "$DOCKERFILE" -t "$IMAGE" .)
 
 docker run --rm -i -v "$here"/..:/ws:ro -v "$old_cwd":/out -e OUTDIR_OWNER=$(id -u) "$IMAGE" /bin/bash -xe /ws/travis/build-centos6.sh
