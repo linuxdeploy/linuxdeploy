639ea456a35e58108ce759fcc4c803e003758742
zaoqi <zaomir@outlook.com>
2019-12-14 00:29:50 +0800

    ARM Support

---
+++ travis/build-docker.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -xe
+
+old_cwd=$(readlink -f .)
+here=$(readlink -f $(dirname "$0"))
+
+DOCKERFILE="$here/Dockerfile.$ARCH"
+IMAGE="linuxdeploy-build-$ARCH"
+
+(cd "$here" && docker build -f "$DOCKERFILE" -t "$IMAGE" .)
+
+docker run --rm -it -v "$here"/..:/ws:ro -v "$old_cwd":/out -e ARCH -e CI=1 -e OUTDIR_OWNER=$(id -u) "$IMAGE" /bin/bash -xe -c "cd /out && /ws/travis/build-in-docker.sh"
