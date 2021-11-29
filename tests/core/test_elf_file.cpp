5685bc575e99fd8fe02878a07532ce069e0940b3
TheAssassin <theassassin@assassinate-you.net>
2021-11-30 00:08:52 +0100

    Test invalid headers and missing files in ELF library
    CC #185.

--- tests/core/test_elf_file.cpp
+++ tests/core/test_elf_file.cpp
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
 #include "linuxdeploy/core/elf_file.h"
 
@@ -8,6 +9,27 @@ namespace bf = boost::filesystem;
 
 using namespace std;
 using namespace linuxdeploy::core::elf_file;
+
+namespace {
+    typedef std::function<void()> callback_t;
+
+    template<typename T>
+    void expectThrowMessageHasSubstr(const callback_t& callback, const char* message) {
+        EXPECT_THAT(callback, ::testing::ThrowsMessage<ElfFileParseError>(::testing::HasSubstr(message)));
+    }
+
+    void expectElfFileConstructorThrowMessage(const char* path, const char* message) {
+        return expectThrowMessageHasSubstr<ElfFileParseError>([=]() { ElfFile{path}; }, message);
+    }
+
+    void expectThrowsElfFileErrorInvalidElfHeader(const char* path) {
+        expectElfFileConstructorThrowMessage(path, "Invalid magic bytes in file header");
+    }
+
+    void expectThrowsElfFileErrorFileNotFound(const char* path) {
+        expectElfFileConstructorThrowMessage(path, "No such file or directory: ");
+    }
+}
 
 namespace LinuxDeployTest {
     class ElfFileTest : public ::testing::Test {};
@@ -26,5 +48,19 @@ namespace LinuxDeployTest {
 
         ElfFile staticLibraryFile(SIMPLE_EXECUTABLE_STATIC_PATH);
         EXPECT_FALSE(staticLibraryFile.isDynamicallyLinked());
+    }
+
+    TEST_F(ElfFileTest, checkInvalidElfHeaderOnEmptyFile) {
+        expectThrowsElfFileErrorInvalidElfHeader("/dev/null");
+    }
+
+    TEST_F(ElfFileTest, checkInvalidElfHeaderOnRandomFiles) {
+        expectThrowsElfFileErrorInvalidElfHeader(SIMPLE_DESKTOP_ENTRY_PATH);
+        expectThrowsElfFileErrorInvalidElfHeader(SIMPLE_ICON_PATH);
+        expectThrowsElfFileErrorInvalidElfHeader(SIMPLE_FILE_PATH);
+    }
+
+    TEST_F(ElfFileTest, checkFileNotFound) {
+        expectThrowsElfFileErrorFileNotFound("/abc/def/ghi/jkl/mno/pqr/stu/vwx/yz");
     }
 }
