0f5361b457b1b6bfc4253570cd2e4049728c018e
TheAssassin <theassassin@assassinate-you.net>
2022-01-11 19:24:08 +0100

    Code style fixes

--- include/linuxdeploy/util/misc.h
+++ include/linuxdeploy/util/misc.h
@@ -56,13 +56,14 @@ namespace linuxdeploy {
                 return split(s, '\n');
             }
 
-            static std::string join(const std::vector<std::string> &strings, const std::string &delimiter)
-            {
+            static std::string join(const std::vector<std::string> &strings, const std::string &delimiter) {
                 std::string result;
                 for (size_t i = 0; i < strings.size(); i++) {
                     result += strings[i];
-                    if (i != strings.size() - 1)
+
+                    if (i != strings.size() - 1) {
                         result += delimiter;
+                    }
                 }
                 return result;
             }
