fd95d72fe89216301ed44664f593a363a2935fe7
Ingo Klöcker <dev@ingo-kloecker.de>
2022-11-15 16:47:43 +0100

    Add missing includes
    gcc 12 requires explicitly including <array> when using std::array.

--- src/plugin/plugin_process_handler.cpp
+++ src/plugin/plugin_process_handler.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <tuple>
 #include <utility>
+#include <array>
 
 // local headers
 #include <linuxdeploy/plugin/plugin_process_handler.h>
--- src/subprocess/subprocess.cpp
+++ src/subprocess/subprocess.cpp
@@ -6,6 +6,7 @@
 #include <utility>
 #include <unistd.h>
 #include <thread>
+#include <array>
 
 // local headers
 #include "linuxdeploy/subprocess/subprocess.h"
