46b11ce6a63d2e1717532ace531c47059cf22fb4
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2023-02-14 16:31:20 +0100

    Handle file access exception while looking for plugins

--- src/plugin/plugin.cpp
+++ src/plugin/plugin.cpp
@@ -37,7 +37,7 @@ namespace linuxdeploy {
 
         std::map<std::string, IPlugin*> findPlugins() {
             std::map<std::string, IPlugin*> foundPlugins;
-
+            
             const auto PATH = getenv("PATH");
 
             auto paths = util::split(PATH, ':');
@@ -58,56 +58,66 @@ namespace linuxdeploy {
             paths.emplace_back(cwd.get());
 
             for (const auto& dir : paths) {
-                if (!fs::is_directory(dir))
-                    continue;
-
-                ldLog() << LD_DEBUG << "Searching for plugins in directory" << dir << std::endl;
-
-                bool extendedDebugLoggingEnabled = (getenv("DEBUG_PLUGIN_DETECTION") != nullptr);
-
-                for (fs::directory_iterator i(dir); i != fs::directory_iterator(); ++i) {
-                    // must be a file, and not a directory
-                    if (fs::is_directory(fs::absolute(*i))) {
-                        if (extendedDebugLoggingEnabled)
-                            ldLog() << LD_DEBUG << "Entry is a directory, skipping:" << i->path() << std::endl;
-
+                try {
+                    if (!fs::is_directory(dir))
                         continue;
-                    }
 
-                    // file must be executable...
-                    static constexpr auto isExecutable = fs::perms::owner_exec | fs::perms::group_exec | fs::perms::others_exec;
-                    if (!(static_cast<unsigned int>(fs::status(*i).permissions()) & static_cast<unsigned int>(isExecutable))) {
-                        if (extendedDebugLoggingEnabled)
-                            ldLog() << LD_DEBUG << "File/symlink is not executable, skipping:" << i->path() << std::endl;
+                    ldLog() << LD_DEBUG << "Searching for plugins in directory" << dir << std::endl;
 
-                        continue;
-                    }
+                    bool extendedDebugLoggingEnabled = (getenv("DEBUG_PLUGIN_DETECTION") != nullptr);
 
-                    // entry name must match regular expression
-                    std::cmatch res;
+                    for (fs::directory_iterator i(dir); i != fs::directory_iterator(); ++i) {
+                        // must be a file, and not a directory
+                        if (fs::is_directory(fs::absolute(*i))) {
+                            if (extendedDebugLoggingEnabled)
+                                ldLog() << LD_DEBUG << "Entry is a directory, skipping:" << i->path() << std::endl;
 
-                    if (!std::regex_match(i->path().filename().string().c_str(), res, PLUGIN_EXPR)) {
-                        ldLog() << LD_DEBUG << "Doesn't match plugin regex, skipping:" << i->path() << std::endl;
-                        continue;
-                    }
-
-                    try {
-                        auto name = res[1].str();
-                        auto* plugin = createPluginInstance(*i);
-                        if (plugin == nullptr) {
-                            ldLog() << LD_DEBUG << "Failed to create instance for plugin" << i->path() << std::endl;;
-                        } else {
-                            ldLog() << LD_DEBUG << "Found plugin '" << LD_NO_SPACE << name << LD_NO_SPACE << "':" << plugin->path() << std::endl;
-
-                            if (foundPlugins.find(name) != foundPlugins.end()) {
-                                ldLog() << LD_DEBUG << "Already found" << name << "plugin in" << foundPlugins[name]->path() << std::endl;
-                            } else {
-                                foundPlugins[name] = plugin;
-                            }
+                            continue;
                         }
-                    } catch (const PluginError& e) {
-                        ldLog() << LD_WARNING << "Could not load plugin" << i->path() << LD_NO_SPACE << ": " << e.what();
+
+                        // file must be executable...
+                        static constexpr auto isExecutable = fs::perms::owner_exec | fs::perms::group_exec | fs::perms::others_exec;
+                        if (!(static_cast<unsigned int>(fs::status(*i).permissions()) & static_cast<unsigned int>(isExecutable))) {
+                            if (extendedDebugLoggingEnabled)
+                                ldLog() << LD_DEBUG << "File/symlink is not executable, skipping:" << i->path() << std::endl;
+
+                            continue;
+                        }
+
+                        // entry name must match regular expression
+                        std::cmatch res;
+
+                        if (!std::regex_match(i->path().filename().string().c_str(), res, PLUGIN_EXPR)) {
+                            ldLog() << LD_DEBUG << "Doesn't match plugin regex, skipping:" << i->path() << std::endl;
+                            continue;
+                        }
+
+                        try {
+                            auto name = res[1].str();
+                            auto* plugin = createPluginInstance(*i);
+                            if (plugin == nullptr) {
+                                ldLog() << LD_DEBUG << "Failed to create instance for plugin" << i->path() << std::endl;;
+                            } else {
+                                ldLog() << LD_DEBUG << "Found plugin '" << LD_NO_SPACE << name << LD_NO_SPACE << "':" << plugin->path() << std::endl;
+
+                                if (foundPlugins.find(name) != foundPlugins.end()) {
+                                    ldLog() << LD_DEBUG << "Already found" << name << "plugin in" << foundPlugins[name]->path() << std::endl;
+                                } else {
+                                    foundPlugins[name] = plugin;
+                                }
+                            }
+                        } catch (const PluginError& e) {
+                            ldLog() << LD_WARNING << "Could not load plugin" << i->path() << LD_NO_SPACE << ": " << e.what();
+                        }
                     }
+                } catch(std::filesystem::filesystem_error const& ex) {
+                    ldLog() << LD_WARNING
+                    << "Got file system error while looking for plugins :  " << ex.what() << '\n'
+                    << "path1(): " << ex.path1() << '\n'
+                    << "path2(): " << ex.path2() << '\n'
+                    << "code().value():    " << ex.code().value() << '\n'
+                    << "code().message():  " << ex.code().message() << '\n'
+                    << "code().category(): " << ex.code().category().name() << '\n';
                 }
             }
 
